88c07b74ca3a36763568d2e76e2a7b047d57b54e
munroe1786 <era3munroe@gmail.com>
2021-05-21 21:15:48 -0500

    correct typo in index.html / move app.js to correct location

---
+++ app.js
@@ -0,0 +1,12 @@
+function imageGallery() {
+    const highlight = document.querySelector(".gallery-highlight");
+    const previews = document.querySelectorAll(".palm-preview img");
+
+    previews.forEach(preview => {
+        preview.addEventListener('click', function() {
+            console.log(this);
+        });
+    });
+}
+
+imageGallery();
