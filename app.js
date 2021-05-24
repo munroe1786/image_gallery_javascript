971a5058d5a6be840ebc085f7716c4f7d6b581f6
munroe1786 <era3munroe@gmail.com>
2021-05-23 21:36:24 -0500

    add to app.js to fade out preview images / revise css file to have same transition values as tutorial

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ function imageGallery() {
     previews.forEach(preview => {
         preview.addEventListener('click', function() {
             const smallSrc = this.src;
-            const largeSrc = smallSrc.replace('small', 'large');
+            const largeSrc = smallSrc.replace("small", "large");
+            previews.forEach(preview => preview.classList.remove("palm-active"));
             highlight.src = largeSrc;
+            preview.classList.add("palm-active");
         });
     });
 }
